e5f4adf754b69341467115612f1472bd33a8466e
aimixsaka <aimixsaka@gmail.com>
2023-12-21 13:50:25 +0800

    rewrite

--- login.sh
+++ login.sh
@@ -5,21 +5,21 @@
 # INFO level message
 ##
 info() {
-  echo -e "\e[32mINFO: $1\e[0m"
+  echo -e "\n\e[32mINFO: $1\e[0m\n"
 }
 
 ##
 # WARN level message
 ##
 warn() {
-  echo -e "\e[33mWARN: $1\e[0m"
+  echo -e "\n\e[33mWARN: $1\e[0m\n"
 }
 
 ##
 # ERROR level message
 ##
 error() {
-  echo -e "\e[31mERROR: $1\e[0m"
+  echo -e "\n\e[31mERROR: $1\e[0m\n"
   exit 1
 }
 
@@ -28,51 +28,31 @@ error() {
 # preconnect to ISP
 ##
 preconnect() {
-  nmcli device wifi rescan
-  sleep 2
-  EXIST=`nmcli device wifi list | grep $ISP`
-  if [ -z "$EXIST" ]; then
-    warn "Can not find $ISP, will retry in 2 seconds"
-    sleep 2
-    # UGLY!
-    EXIST=`nmcli device wifi list | grep $ISP`
-    if [ -z "$EXIST" ]; then
-      error "Not exist: $ISP"
+    if ! nmcli device wifi connect $ISP; then
+      nmcli device wifi rescan
+      sleep 2
+      nmcli device wifi connect $ISP ||
+        error "Cannot connect to $ISP"
     fi
-  else
-    # no need for error handling
-    nmcli device wifi connect $ISP > /dev/null
-    info "Preconnected Successfully"
-  fi
 }
 
 ##
 # Preset some variable after preconnect
 ##
 preset() {
-  # magic variable(
-  WLANACIP="10.255.252.150"
-  WLANACNAME="XL-BRAS-SR8806-X"
-  
-  # get pc ip
-  IP="$(nmcli connection show NJUPT-CMCC | grep "IP4.ADDRESS" | awk '{print $2}' | cut -d/ -f 1)"
-  
-  # Deprecated(use command parm instead)
-  # get operator connected
-  #isp=$(nmcli -t -f ACTIVE,SSID dev wifi | grep yes | cut -d: -f2)
-  
+  IP=$(ip --json addr show dev wlp0s20f3 | jq '.[0].addr_info.[0].local' | tr -d \")
   case $ISP in
     "NJUPT-CHINANET")
       info "Detected that you are using 'NJUPT-CHINANET'"
-      LOGIN_ID="%2C0%2C${USERNAME}%40njxy"
+      LOGIN_ID="$USERNAME@njxy"
       ;;
     "NJUPT-CMCC")
       info "Detected that you are using 'NJUPT-CMCC'"
-      LOGIN_ID="%2C0%2C${USERNAME}%40cmcc"
+      LOGIN_ID="$USERNAME@cmcc"
       ;;
     "NJUPT")
       info "Detected that you are using 'NJUPT'"
-      LOGIN_ID="%2C0%2C${USERNAME}"
+      LOGIN_ID="$USERNAME"
       ;;
     *)
       error "Cannot recognize wifi name!"
@@ -83,30 +63,29 @@ usage() {
   echo "Usage: ./login -U \"username\" -P \"password\" -I \"isp\""
   echo "Login to NJUPT internet"
   echo
-  echo "  -h,--help             output help message"
-  echo "  -I,--isp              which isp to connect(NJUPT,NJUPT-CMCC,NJUPT-CHINANET)"
-  echo "  -U,--username         login username, usually your student id"
-  echo "  -P,--password         login password"
+  echo "  -h, --help             output help message"
+  echo "  -I, --isp              which isp to connect(NJUPT,NJUPT-CMCC,NJUPT-CHINANET)"
+  echo "  -U, --username         login username, usually your student id"
+  echo "  -P, --password         login password"
 }
 
 login() {
-  curl "http://10.10.244.11:801/eportal/?c=ACSetting&a=Login&protocol=http:&hostname=10.10.244.11&iTermType=1&wlanuserip=${IP}&wlanacip=${WLANACIP}&wlanacname=${WLANACNAME}&mac=00-00-00-00-00-00&ip=${IP}&enAdvert=0&queryACIP=0&loginMethod=1" \
-    --data "DDDDD=${LOGIN_ID}&upass=${PASSWORD}&R1=0&R2=0&R3=0&R6=0&para=00&0MKKey=123456&buttonClicked=&redirect_url=&err_flag=&username=&password=&user=&cmd=&Login=&v6ip="
+  curl -k \
+    --request GET \
+    --connect-timeout 5 \
+    "https://10.10.244.11:802/eportal/portal/login?callback=dr1003&login_method=1&user_account=,0,${LOGIN_ID}&user_password=${PASSWORD}&wlan_user_ip=${IP}&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name="
 
   if curl -s --head --connect-timeout 3 https://www.baidu.com | head -n 1 | grep "HTTP/1.[01] [23].." > /dev/null; then
     info "Connected"
   else
-    error "Connect Failed! Check your username or password"
+    error "Connect Failed, is it the fault of magic?"
   fi
 }
 
 ##
 # Start
 ##
-# precheck
-
-while [ "$#" -ne "0" ]
-do
+while (( $# )); do
   case "$1" in 
     -h | --help | "") usage; exit 0;  ;;
     -I | --isp)       ISP="$2";       shift 2;;
@@ -117,17 +96,8 @@ do
   esac
 done
 
-if [ -z "$PASSWORD" ] || [ -z "$USERNAME" ] || [ -z "$ISP" ]; then
+if [[ -z "$PASSWORD" ]] || [[ -z "$USERNAME" ]] || [[ -z "$ISP" ]]; then
   error "Missing Params!"
-fi
-
-# precheck if already connected
-curl -s --head --connect-timeout 3 https://www.baidu.com | head -n 1 | grep "HTTP/1.[01] [23].." > /dev/null
-SUCCESS="$?"
-CURRENT_ISP=`nmcli -t -f ACTIVE,SSID dev wifi | grep yes | cut -d: -f2`
-if [ "$SUCCESS" -eq "0" ] && [ $ISP = $CURRENT_ISP ]; then
-  warn "You have already connected to $ISP"
-  exit 0
 fi
 
 # preconnect
